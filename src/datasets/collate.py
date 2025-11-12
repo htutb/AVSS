fa4dcc90dbc6a88b7f3d9f1f15dc6c66e742e4bc
Kirill <priborakirill276@gmail.com>
2025-11-13 02:54:37 +0300

    base dataset + undone collate

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -1,5 +1,5 @@
 import torch
-
+from torch.nn.utils.rnn import pad_sequence
 
 def collate_fn(dataset_items: list[dict]):
     """
@@ -14,4 +14,37 @@ def collate_fn(dataset_items: list[dict]):
             of the tensors.
     """
 
-    pass  # TODO
+    s1_specs = [item["s1_spectrogram"] for item in dataset_items]
+    s2_specs = [item["s2_spectrogram"] for item in dataset_items]
+    mix_specs = [item["mix_spectrogram"] for item in dataset_items]
+
+    s1_spec_lengths = torch.tensor([s.shape[0] for s in s1_specs], dtype=torch.long)
+    s2_spec_lengths = torch.tensor([s.shape[0] for s in s2_specs], dtype=torch.long)
+    mix_spec_lengths = torch.tensor([s.shape[0] for s in mix_specs], dtype=torch.long)
+
+    s1_spec_batch = pad_sequence(s1_specs, batch_first=True, padding_value=0.0)
+    s2_spec_batch = pad_sequence(s2_specs, batch_first=True, padding_value=0.0)  
+    mix_spec_batch = pad_sequence(mix_specs, batch_first=True, padding_value=0.0)
+
+    s1_audios = torch.tensor([item["s1_audio"] for item in dataset_items])
+    s2_audios = torch.tensor([item["s2_audio"] for item in dataset_items])
+    mix_audios = torch.tensor([item["mix_audio"] for item in dataset_items])
+
+    s1_mouths = torch.tensor([item["mouth1"] for item in dataset_items])
+    s2_mouths = torch.tensor([item["mouth2"] for item in dataset_items])  
+
+    # audio_paths = [item['audio_path'] for item in dataset_items]
+        
+    return {
+    's1_spectrogram': s1_spec_batch,
+    's2_spectrogram': s2_spec_batch,
+    'mix_spectrogram': mix_spec_batch,
+    's1_spectrogram_lenghts': s1_spec_lengths,
+    's2_spectrogram_lenghts': s2_spec_lengths,
+    'mix_spectrogram_lenghts': mix_spec_lengths,
+    's1_audio': s1_audios,
+    's2_audio': s2_audios,
+    'mix_audio': mix_audios,
+    's1_mouth': s1_mouths,
+    's2_mouth': s2_mouths,
+    }
